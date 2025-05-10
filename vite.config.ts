a351d4eeaac85264c305d67c2fa091d4e707741b
Vishnu <vishnu.p@alldirectionsource.com>
2025-05-10 14:13:55 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-portfolio/', // 👈 Add this line
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
